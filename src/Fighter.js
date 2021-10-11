e1ebd962a4f52a9d9c09c8c09c806b167498634a
RL2020 <loc.fitness1@gmail.com>
2021-10-11 16:21:24 -0400

    first commit

---
+++ src/Fighter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+class Fighter extends React.Component {
+    render() {
+        return <div>Welcome to FightCard!!!</div>
+    }
+    
+}
+
+export default Fighter 
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+import Fighter from "./Fighter";
+
+ReactDOM.render(
+    <Fighter /> ,
+    document.getElementById("root")
+  );
