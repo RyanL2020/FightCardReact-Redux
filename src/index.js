7c7e117caa07ece5f33d1603752b4cee391f65a0
RL2020 <loc.fitness1@gmail.com>
2021-10-27 17:04:17 -0400

    installed redux react-redux axios redux-thunk

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import App from './components/App';
 
 
 ReactDOM.render(
-       
     <App />, 
-    document.querySelector('#root'));
+    document.querySelector('#root')
+);
+       
     
         
